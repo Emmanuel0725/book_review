b3dff3c35652764ded608ea25e5a5e293c2a489e
Emmanuel0725 <emmanuelfrancismacaya@gmail.com>
2023-07-13 03:02:29 +0800

    Add project files.

---
+++ book_review/SendCodeWindow1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace book_review
+{
+    /// <summary>
+    /// Interaction logic for SendCodeWindow1.xaml
+    /// </summary>
+    public partial class SendCodeWindow1 : Window
+    {
+        public SendCodeWindow1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
